6a4d0d88acc88986b385e7c95e5e8dc052fd955e
Charlesccharris <harriscc@student.swosu.edu>
2020-09-23 11:10:36 -0500

    Added output for all problems

--- Homework5_2.java
+++ Homework5_2.java
@@ -8,11 +8,16 @@ class Homework5_2{
 	Display the correct count and test time.*/
 
 	Scanner input = new Scanner(System.in);
+	int round = 0;
+	String done = " ";
+
+	for(round = 0; round < 10; round++){
 	int number1 = (int)(Math.random() * 100);
 	int number2 = (int)(Math.random() * 100);
 	number1 = number1 % 15 + 1;
 	number2 = number2 % 15 + 1;
 	int temp = 0;
+
 	if(number1 < number2){
 	temp = number1;
 	number1 = number2;
@@ -29,5 +34,8 @@ class Homework5_2{
 	else
 		System.out.println("Incorrect");
 
+	done += "\n" + number1 + "-" + number2 + "=" + guess + ((number1 - number2 == guess) ? " correct" : " wrong");
+	}
+	System.out.println(done);
 	}
 }
