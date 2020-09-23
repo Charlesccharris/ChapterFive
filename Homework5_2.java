30f0a2f29ca9eb630c6e4b36067f264b0633e397
Charlesccharris <harriscc@student.swosu.edu>
2020-09-22 23:57:09 -0500

    Got the framework done

--- Homework5_2.java
+++ Homework5_2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Homework5_2{
 	public static void main(String[] args){
 	/*(Repeat additions)
@@ -5,13 +7,23 @@ class Homework5_2{
 	Revise the program to generate ten random addition questions for two integers between 1 and 15.
 	Display the correct count and test time.*/
 
-	int number1 = 4;
-	int number2 = 2;
+	Scanner input = new Scanner(System.in);
+	int number1 = (int)(Math.random() * 100);
+	int number2 = (int)(Math.random() * 100);
+	number1 = number1 % 15 + 1;
+	number2 = number2 % 15 + 1;
+	int temp = 0;
+	if(number1 < number2){
+	temp = number1;
+	number1 = number2;
+	number2 = temp;
+	}
 
-	System.out.println("What is " + number1 + " - " + number2 + "?");
+	System.out.print("What is " + number1 + " - " + number2 + "? ");
 
 	int answer = number1 - number2;
-	int guess = 2;
+	int guess = input.nextInt();
+
 	if(guess == answer)
 		System.out.println("Correct");
 	else
--- Homework5_40.java
+++ Homework5_40.java
@@ -2,7 +2,21 @@ class Homework5_40{
 	public static void main(String[] args){
 	/*(Simulation: heads or tails)
 	Write a program that simulates flipping a coin one million times and displays the number of heads and tails.*/
+	int rounds = 0;
+	int heads = 0;
+	int tails = 0;
+	double headsOrTails = 0;
 
-	System.out.println("Bob");
+	for(rounds = 0; rounds < 1000000; rounds++){
+		headsOrTails = Math.random();
+
+		if(headsOrTails < 0.5)
+		tails++;
+		else
+		heads++;
+	}
+
+	System.out.println("It was " + tails + " tails and " + heads + " heads!");
+
 	}
 }
