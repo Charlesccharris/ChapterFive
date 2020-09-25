4995ea36d473d72e1945874101dadea6db66edaa
Charlesccharris <harriscc@student.swosu.edu>
2020-09-25 00:30:27 -0500

    i have no idea what the heck I'm doing. I just made a giant mess

--- Homework5_2.java
+++ Homework5_2.java
@@ -18,18 +18,22 @@ class Homework5_2{
 	totalRounds = input.nextInt();
 
 	for(round = 0; round < totalRounds; round++){
+
 	int number1 = (int)(Math.random() * 100);
 	int number2 = (int)(Math.random() * 100);
 	number1 = number1 % 15 + 1;
 	number2 = number2 % 15 + 1;
 	int temp = 0;
 
-	if(number1 < number2){
+	Swap swap1 = new Swap();
+	}
+
+/*	if(number1 < number2){
 	temp = number1;
 	number1 = number2;
 	number2 = temp;
 	}
-
+*/
 	System.out.print("What is " + number1 + " - " + number2 + "? ");
 
 	int answer = number1 - number2;
@@ -43,10 +47,47 @@ class Homework5_2{
 		System.out.println("Incorrect");
 
 	done += "\n" + number1 + "-" + number2 + "=" + guess + ((number1 - number2 == guess) ? " correct" : " wrong");
-	}
+
 	long endTime = System.currentTimeMillis();
 	long totalTime = endTime - startTime;
 
 	System.out.println("Number of questions answered right: " + wins + "\nTest time was " + totalTime/1000 + " seconds\n" +done);
-	}
 }
+
+void Swap(
+        int number1 = (int)((Math.random() * 100) % 15 + 1);
+        int number2 = (int)((Math.random() * 100) % 15 + 1);
+        int temp = 0;
+
+	public void getNumbers() {
+        if(number1 < number2){
+        temp = number1;
+        number1 = number2;
+        number2 = temp;
+	}
+)
+}
+}
+/*        System.out.print("What is " + number1 + " - " + number2 + "? ");
+
+        int answer = number1 - number2;
+        int guess = input.nextInt();
+
+        if(guess == answer){
+                System.out.println("Correct");
+                wins++;
+        }
+        else
+                System.out.println("Incorrect");
+
+        done += "\n" + number1 + "-" + number2 + "=" + guess + ((number1 - number2 == guess) ? " correct" : " wrong");
+        }
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+
+        System.out.println("Number of questions answered right: " + wins + "\nTest time was " + totalTime/1000 + " seconds\n" +done);
+
+
+	return swap;
+     	}
+*/
