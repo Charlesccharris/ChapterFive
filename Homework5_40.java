1ca77e2b364240c476adfe88e1e2f481d9aafc74
Charlesccharris <harriscc@student.swosu.edu>
2020-09-24 23:39:46 -0500

    Added the ability to choose the multiplier and the initial number

--- Homework5_40.java
+++ Homework5_40.java
@@ -1,22 +1,38 @@
+import java.util.Scanner;
+
 class Homework5_40{
 	public static void main(String[] args){
 	/*(Simulation: heads or tails)
 	Write a program that simulates flipping a coin one million times and displays the number of heads and tails.*/
+	Scanner input = new Scanner(System.in);
 
         int heads = 0;
         int tails = 0;
         int flips = 0;
+	int totalFlips = 0;
 
-        System.out.println("This program will flip a coin 1,000,000 times, and display total number of heads and tails");
-        while(flips < 1000000){
-        int coin = (int)(Math.random() * 10 % 2);
-	        if(coin == 0){
-	        heads++;
-	        flips++;
+	System.out.print("Insert a number: ");
+	totalFlips = input.nextInt();
+
+	System.out.print("Now choose a multiplier, (H)undreds, (T)housands, (M)illions: ");
+	char choice = input.next().charAt(0);
+	choice = Character.toUpperCase(choice);
+
+	if(choice == 'H')
+		totalFlips *= 100;
+	else if(choice == 'T')
+		totalFlips *= 1000;
+	else if(choice == 'M')
+		totalFlips *= 1000000;
+	else{
+		System.out.println("NO");
+		System.exit(1);
+	}
+        System.out.println("This program will flip a coin " + totalFlips + " times, and display total number of heads and tails"); while(flips < totalFlips){ int 
+        coin = (int)(Math.random() * 10 % 2);
+	        if(coin == 0){ heads++; flips++;
 	        }
-	        else if(coin == 1){
-	        tails++;
-	        flips++;
+	        else if(coin == 1){ tails++; flips++;
 	        }
         }
         System.out.println("It was " + heads + " heads and " + tails + " tails");
