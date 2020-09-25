2c9a575c23d99a77fa1067ad36776a4a896ba540
Charlesccharris <70536520+Charlesccharris@users.noreply.github.com>
2020-09-25 08:53:50 -0500

    Update Homework5_2.java
    Fixed the code, and moved the timer

--- Homework5_2.java
+++ Homework5_2.java
@@ -9,31 +9,28 @@ class Homework5_2{
 
 	Scanner input = new Scanner(System.in);
 	int round = 0;
-	long startTime = System.currentTimeMillis();
 	String done = " ";
 	int wins = 0;
 	int totalRounds = 0;
 
 	System.out.print("How many rounds of subtraction: ");
 	totalRounds = input.nextInt();
+		
+	long startTime = System.currentTimeMillis();
 
 	for(round = 0; round < totalRounds; round++){
-
 	int number1 = (int)(Math.random() * 100);
 	int number2 = (int)(Math.random() * 100);
 	number1 = number1 % 15 + 1;
 	number2 = number2 % 15 + 1;
 	int temp = 0;
 
-	Swap swap1 = new Swap();
-	}
-
-/*	if(number1 < number2){
+	if(number1 < number2){
 	temp = number1;
 	number1 = number2;
 	number2 = temp;
 	}
-*/
+
 	System.out.print("What is " + number1 + " - " + number2 + "? ");
 
 	int answer = number1 - number2;
@@ -47,47 +44,10 @@ class Homework5_2{
 		System.out.println("Incorrect");
 
 	done += "\n" + number1 + "-" + number2 + "=" + guess + ((number1 - number2 == guess) ? " correct" : " wrong");
-
+	}
 	long endTime = System.currentTimeMillis();
 	long totalTime = endTime - startTime;
 
 	System.out.println("Number of questions answered right: " + wins + "\nTest time was " + totalTime/1000 + " seconds\n" +done);
-}
-
-void Swap(
-        int number1 = (int)((Math.random() * 100) % 15 + 1);
-        int number2 = (int)((Math.random() * 100) % 15 + 1);
-        int temp = 0;
-
-	public void getNumbers() {
-        if(number1 < number2){
-        temp = number1;
-        number1 = number2;
-        number2 = temp;
 	}
-)
 }
-}
-/*        System.out.print("What is " + number1 + " - " + number2 + "? ");
-
-        int answer = number1 - number2;
-        int guess = input.nextInt();
-
-        if(guess == answer){
-                System.out.println("Correct");
-                wins++;
-        }
-        else
-                System.out.println("Incorrect");
-
-        done += "\n" + number1 + "-" + number2 + "=" + guess + ((number1 - number2 == guess) ? " correct" : " wrong");
-        }
-        long endTime = System.currentTimeMillis();
-        long totalTime = endTime - startTime;
-
-        System.out.println("Number of questions answered right: " + wins + "\nTest time was " + totalTime/1000 + " seconds\n" +done);
-
-
-	return swap;
-     	}
-*/
